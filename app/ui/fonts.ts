f8633d48e2cb595c45407e9ef406238f5f5a508f
Monica Kornis <monicakornis@Monicas-MBP.lan>
2024-09-29 14:58:07 -0400

    adding files for next dashboard

---
+++ app/ui/fonts.ts
@@ -0,0 +1,3 @@
+import { Gowun_Batang, Lusitana } from 'next/font/google';
+export const gowunbatang = Gowun_Batang({subsets: ['latin'], weight: '400'});
+export const lusitana = Lusitana({subsets: ['latin'], weight: '700'})
